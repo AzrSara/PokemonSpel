f89fe8067b5ac93e5805a84083de0b275c722a93
AzrSara <137381182+AzrSara@users.noreply.github.com>
2023-12-17 15:33:57 +0100

    fixade reserv pokemon

--- pokemon.js
+++ pokemon.js
@@ -5,26 +5,25 @@ document.addEventListener('DOMContentLoaded', function () {
     const pokemonImage = document.getElementById('pokemonImage');
     const pokemonName = document.getElementById('pokemonName');
     const addToTeamButton = document.getElementById('addToTeamButton');
+    const addToReserveButton = document.getElementById('addToReserveButton');
     const overlayContainer = document.getElementById('overlay-container');
     const messageContainer = document.getElementById('messageContainer');
     const nicknameInput = document.getElementById('nicknameInput');
     const team = [];
+    const reserve = [];
 
     function showMessage(message, isError = false, isRemainingPokemonMessage = false) {
         messageContainer.textContent = message;
 
         if (isError) {
-            // Apply styles for error messages
-            messageContainer.style.backgroundColor = '#f44336'; // Background color for error messages
-            messageContainer.style.color = 'white'; // Text color for error messages
+            messageContainer.style.backgroundColor = '#f44336';
+            messageContainer.style.color = 'white';
         } else if (isRemainingPokemonMessage) {
-            // Apply styles for remaining Pokemon messages
-            messageContainer.style.backgroundColor = '#333'; // Background color for remaining Pokemon messages
-            messageContainer.style.color = '#ff9800'; // Text color for remaining Pokemon messages
+            messageContainer.style.backgroundColor = '#333';
+            messageContainer.style.color = '#ff9800';
         } else {
-            // Reset styles for regular messages
-            messageContainer.style.backgroundColor = '#4CAF50'; // Background color for regular messages
-            messageContainer.style.color = 'white'; // Text color for regular messages
+            messageContainer.style.backgroundColor = '#4CAF50';
+            messageContainer.style.color = 'white';
         }
 
         messageContainer.style.display = 'block';
@@ -58,6 +57,25 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    function updateReserveList() {
+        reserveList.innerHTML = '';
+
+        reserve.forEach((pokemon, index) => {
+            const pokemonBox = document.createElement('div');
+            pokemonBox.classList.add('pokemon-box-overlay');
+
+            pokemonBox.innerHTML = `<p>${pokemon.nickname ? `${pokemon.nickname} (${pokemon.name})` : pokemon.name}</p>
+                                <img src="${pokemon.image}" alt="${pokemon.name} Image">`;
+
+            pokemonBox.addEventListener('click', () => {
+                reserve.splice(index, 1);
+                updateReserveList();
+            });
+
+            reserveList.appendChild(pokemonBox);
+        });
+    }
+
     function updateOverlayContent() {
         const overlay = document.getElementById('overlay');
         overlay.innerHTML = '';
@@ -78,6 +96,46 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
+    addToReserveButton.addEventListener('click', function () {
+        const selectedPokemon = pokemonName.textContent;
+        const selectedPokemonImage = pokemonImage.src;
+        const nickname = nicknameInput.value.trim();
+
+        const newPokemon = { name: selectedPokemon, nickname: nickname, image: selectedPokemonImage };
+        reserve.push(newPokemon);
+        updateReserveList();
+    });
+
+    addToTeamButton.addEventListener('click', function () {
+        const selectedPokemon = pokemonName.textContent;
+        const selectedPokemonImage = pokemonImage.src;
+        const nickname = nicknameInput.value.trim();
+
+        if (team.length < 3) {
+            const newPokemon = { name: selectedPokemon, nickname: nickname, image: selectedPokemonImage };
+            team.push(newPokemon);
+            updatePokemonBoxes();
+
+            overlayContainer.style.display = team.length > 0 ? 'flex' : 'none';
+
+            if (team.length < 3) {
+                const remainingPokemon = 3 - team.length;
+                showMessage(`Add ${remainingPokemon} more Pokémon to complete your team.`, false, true);
+            } else {
+                showMessage('Team is full. Remove a Pokémon before adding more.');
+            }
+        }
+    });
+
+    document.addEventListener('click', event => {
+        if (event.target === overlayContainer) {
+            overlayContainer.style.display = 'none';
+            if (team.length === 0) {
+                pokemonDetails.style.display = 'none';
+            }
+        }
+    });
+
     fetchPokemonData('https://pokeapi.co/api/v2/pokemon?limit=151')
         .then(data => {
             const pokemonNames = data.results.map(pokemon => pokemon.name).sort();
@@ -95,35 +153,10 @@ document.addEventListener('DOMContentLoaded', function () {
                         pokemonImage.style.height = '130px';
                     });
             });
-
-            addToTeamButton.addEventListener('click', function () {
-                const selectedPokemon = pokemonName.textContent;
-                const selectedPokemonImage = pokemonImage.src;
-                const nickname = nicknameInput.value.trim();
-
-                if (team.length < 3) {
-                    const newPokemon = { name: selectedPokemon, nickname: nickname, image: selectedPokemonImage };
-                    team.push(newPokemon);
-                    updatePokemonBoxes();
-
-                    overlayContainer.style.display = team.length > 0 ? 'flex' : 'none';
-
-                    if (team.length < 3) {
-                        const remainingPokemon = 3 - team.length;
-                        showMessage(`Add ${remainingPokemon} more Pokémon to complete your team.`, false, true);
-                    } else {
-                        showMessage('Team is full. Remove a Pokémon before adding more.');
-                    }
-                }
-            });
-
-            document.addEventListener('click', event => {
-                if (event.target === overlayContainer) {
-                    overlayContainer.style.display = 'none';
-                    if (team.length === 0) {
-                        pokemonDetails.style.display = 'none';
-                    }
-                }
-            });
         });
 });
+
+
+  
+
+
